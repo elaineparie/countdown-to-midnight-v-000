22242341c9e4f6ef0533cc590802bbad9e7fb1d9
Elaine Anderson <eparie@bu.edu>
2018-05-18 21:14:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,9 +8,9 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
+def countdown_with_sleep(number.sleep 1)
   while number > 0
-    puts "#{number} SECOND(S)!".sleep 1
+    puts "#{number} SECOND(S)!"
    number -= 1
   end
 end
